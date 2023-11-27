819a3c2268ee6c60d59e44658084d76358a33b99
Angela Carvalho <angelalopo@outlook.com>
2023-11-27 18:07:53 -0300

    insere o projeto final com as devidas alterações

---
+++ js/calculadora.js
@@ -0,0 +1,47 @@
+function somar() {
+    const numero1 = parseFloat(document.getElementById("numero1Soma").value);
+    const numero2 = parseFloat(document.getElementById("numero2Soma").value);
+    // evite mexer no código acima!
+  
+    //escreva seu código aqui
+  
+    /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
+    Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
+    document.getElementById("resultadoSoma").innerHTML = resultado;
+}
+
+function subtrair() {
+    const numero1 = parseFloat(document.getElementById("numero1Sub").value);
+    const numero2 = parseFloat(document.getElementById("numero2Sub").value);
+    // evite mexer no código acima!
+
+    //escreva seu código aqui
+
+    /* use as constantes numero 1 e numero 2 e realize a subtração entre elas.
+        Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
+    document.getElementById("resultadoSub").innerHTML = resultado;
+}
+
+function multiplicar() {
+    const numero1 = parseFloat(document.getElementById("numero1Mult").value);
+    const numero2 = parseFloat(document.getElementById("numero2Mult").value);
+    // evite mexer no código acima!
+
+    //escreva seu código aqui
+
+    /* use as constantes numero 1 e numero 2 e realize a multiplicação entre elas.
+        Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
+    document.getElementById("resultadoMult").innerHTML = resultado;
+}
+
+function dividir() {
+    const numero1 = parseFloat(document.getElementById("numero1Div").value);
+    const numero2 = parseFloat(document.getElementById("numero2Div").value);
+    // evite mexer no código acima!
+
+    //escreva seu código aqui
+
+    /* use as constantes numero 1 e numero 2 e realize a divisão entre elas.
+        Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
+    document.getElementById("resultadoDiv").innerHTML = resultado;
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# [Ciclo Formativo Básico em Tecnologia] Projeto final do módulo de JavaScript:
+
+
+Bem vindas ao repositório do projeto final do módulo de JavaScript.
+
+  1. Faça um fork desse repositório.
+  2. Abra o arquivo `calculadora.js` e siga as instruções para para que as operações básicas passem a ser funcionais.
+  3. Abra o arquivo `media.js` e siga as instruções para para que a soma passe a ser funcional.  
+  4. [obrigatório] Por fim, publique no Netlify!
+  
